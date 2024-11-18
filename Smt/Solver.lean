ae25a1fa3cfd42a0d3fbaab9ad08dc6a7a1091ee
Lizn-zn <lizenan1995@foxmail.com>
2024-11-18 13:15:56 +0000

    update to v4.13.0

---
+++ Smt/Solver.lean
@@ -0,0 +1,210 @@
+/-
+Copyright (c) 2021-2022 by the authors listed in the file AUTHORS and their
+institutional affiliations. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Abdalrhman Mohamed
+-/
+
+import Lean
+import Std.Data.HashMap
+import Smt.Reconstruct
+import Smt.Translate.Query
+
+namespace Smt
+
+open Lean hiding Command
+open Translate
+
+inductive Kind where
+  --| boolector
+  | cvc5
+  | vampire
+  --| yices
+  | z3
+  | sysol
+  | syopt
+  | mplbt
+  | mplrc
+  | mmard
+  | mmafi
+
+deriving DecidableEq, Inhabited, Hashable
+
+def allKinds : List Kind := [
+  -- Kind.boolector,
+  Kind.cvc5,
+  Kind.vampire,
+  -- Kind.yices,
+  Kind.z3,
+  Kind.sysol,
+  Kind.syopt,
+  Kind.mplbt,
+  Kind.mplrc,
+  Kind.mmard,
+  Kind.mmafi
+]
+
+instance : ToString Kind where
+  toString
+    --| .boolector => "boolector"
+    | .cvc5      => "cvc5"
+    | .vampire   => "vampire"
+    --| .yices     => "yices"
+    | .z3        => "z3"
+    | .sysol     => "sysol"
+    | .syopt     => "syopt"
+    | .mplbt     => "mplbt"
+    | .mplrc     => "mplrc"
+    | .mmard     => "mmard"
+    | .mmafi     => "mmafi"
+
+instance : Lean.KVMap.Value Kind where
+  toDataValue k := toString k
+  ofDataValue?
+    --| "boolector" => Kind.boolector
+    | "cvc5"      => Kind.cvc5
+    | "vampire"   => Kind.vampire
+    --| "yices"     => Kind.yices
+    | "z3"        => Kind.z3
+    | "sysol"     => Kind.sysol
+    | "syopt"     => Kind.syopt
+    | "mplbt"     => Kind.mplbt
+    | "mplrc"     => Kind.mplrc
+    | "mmarc"     => Kind.mmard
+    | "mmafi"     => Kind.mmafi
+    | _           => none
+
+/-- What the binary for a given solver is usually called. -/
+def Kind.toDefaultPath : Kind → String
+  --| .yices => "yices-smt2"
+  | k      => toString k
+
+inductive Result where
+  | sat : String → Result
+  | unsat : String → Result
+  | unknown : String → Result
+  | except  : String → Result
+  | timeout : String → Result
+deriving DecidableEq, Inhabited
+
+instance : ToString Result where
+  toString : Result → String
+    | .sat msg     => "sat. " ++ msg
+    | .unsat msg   => "unsat. " ++ msg
+    | .unknown msg => "unknown. " ++ msg
+    | .except msg  => "except. " ++ msg
+    | .timeout msg => "timeout. " ++ msg
+
+/-- The data-structure for the state of the generic SMT-LIB solver. -/
+structure SolverState where
+  commands : List Command
+  args : Std.HashMap Kind (Array String)
+
+abbrev SolverT (m) [Monad m] [MonadLiftT IO m] := StateT SolverState m
+
+abbrev SolverM := SolverT IO
+
+namespace Solver
+
+variable [Monad m] [MonadLiftT IO m] [MonadLiftT BaseIO m]
+
+def addCommand (c : Command) : SolverT m Unit := do
+  let state ← get
+  set { state with commands := c :: state.commands }
+
+def addCommands : List Command → SolverT m Unit := (List.forM · addCommand)
+
+/-- Create an instance of a pre-configured SMT solver. -/
+def create (timeoutSecs : Nat) (solvers : List Kind): IO SolverState := do
+  let allArgs : Std.HashMap Kind (Array String) := Std.HashMap.ofList [
+    (.cvc5,      #["--timeout", toString timeoutSecs]),
+    -- (.vampire,   #["--input_syntax", "smtlib2", "--output_mode", "smtcomp", "--time_limit", toString timeoutSecs]),
+    --(.yices,     #["--timeout", toString timeoutSecs]),
+    (.z3,        #["--timeout", toString timeoutSecs]),
+    (.sysol,     #["--timeout", toString timeoutSecs]),
+    (.syopt,     #["--timeout", toString timeoutSecs]),
+    (.mplbt,     #["--timeout", toString timeoutSecs]),
+    (.mplrc,     #["--timeout", toString timeoutSecs]),
+    (.mmard,     #["--timeout", toString timeoutSecs]),
+    (.mmafi,     #["--timeout", toString timeoutSecs]),
+  ]
+  let args := solvers.foldl (fun acc solver =>
+    match allArgs.get? solver with
+    | some args => acc.insert solver args
+    | none => acc
+  ) Std.HashMap.empty
+  return ⟨[], args⟩
+
+/-- Set the SMT query logic to `l`. -/
+def setLogic (l : String) : SolverT m Unit := addCommand (.setLogic l)
+
+/-- Set option `k` to value `v`. -/
+def setOption (k : String) (v : String) : SolverT m Unit := addCommand (.setOption k v)
+
+/-- Define a sort with name `id` and arity `n`. -/
+def declareSort (id : String) (n : Nat) : SolverT m Unit := addCommand (.declareSort id n)
+
+/-- Define a sort with name `id`. -/
+def defineSort (id : String) (ss : List Term) (s : Term) : SolverT m Unit :=
+  addCommand (.defineSort id ss s)
+
+/-- Declare a symbolic constant with name `id` and sort `s`. -/
+def declareConst (id : String) (s : Term) : SolverT m Unit := addCommand (.declare id s)
+
+/-- Declare an uninterpreted function with name `id` and sort `s`. -/
+def declareFun (id : String) (s : Term) : SolverT m Unit := addCommand (.declare id s)
+
+/-- Define a function with name `id`, parameters `ps`, co-domain `s`,
+    and body `t`. -/
+def defineFun (id : String) (ps : List (String × Term)) (s : Term) (t : Term) :
+  SolverT m Unit := addCommand (.defineFun id ps s t false)
+
+/-- Define a recursive function with name `id`, parameters `ps`, co-domain `s`,
+    and body `t`. -/
+def defineFunRec (id : String) (ps : List (String × Term)) (s : Term) (t : Term) :
+  SolverT m Unit := addCommand (.defineFun id ps s t true)
+
+/-- Assert that proposition `t` is true. -/
+def assert (t : Term) : SolverT m Unit := addCommand (.assert t)
+
+/-- Check if the query given so far is satisfiable and return the result. -/
+def checkSat : SolverT m Result := do
+  let state ← get
+
+  let mut args : Array String := #[]
+
+  for kind in allKinds do
+    match state.args.get? kind with
+    | some argArray =>
+        args := args.push s!"--{kind.toDefaultPath}"
+        let combinedArgs := argArray.foldl (fun r s => r ++ " " ++ s) ""
+        args := args.push combinedArgs
+    | none => continue
+
+  let proc ← IO.Process.spawn {
+    cmd := "mtsolve"
+    args := args
+    stdin := .piped
+    stdout := .piped
+    stderr := .piped
+  }
+
+  for c in state.commands.reverse ++ [.exit] do
+    proc.stdin.putStr s!"{c}\n"
+
+  proc.stdin.flush
+  let (_, proc) ← proc.takeStdin
+  let _ ← proc.wait
+
+  let res := (← proc.stdout.readToEnd).trim
+  let msg := (← proc.stderr.readToEnd).trim
+
+  match res with
+  | "sat"     => return (.sat msg)
+  | "unsat"   => return (.unsat msg)
+  | "unknown" => return (.unknown msg)
+  | "timeout" => return (.timeout msg)
+  | "except"  => return (.except msg)
+  | _ => (throw (IO.userError s!"unexpected solver output\nstdout: {res}\nstderr: {msg}") : IO _)
+
+end Smt.Solver
